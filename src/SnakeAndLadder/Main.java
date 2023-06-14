745c36b61b6930eff8227d32b63d5f231e04c978
Maaz <maazshuaib18@gmail.com>
2023-06-14 21:37:53 +0530

    I/O

--- src/SnakeAndLadder/Main.java
+++ src/SnakeAndLadder/Main.java
@@ -1,7 +1,37 @@
 package SnakeAndLadder;
 
+import SnakeAndLadder.model.Levels;
+import SnakeAndLadder.service.Play;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("1");
+        Scanner sc = new Scanner(System.in);
+        Levels levels = new Levels();
+        int numberOfSnakes = sc.nextInt();
+        while (numberOfSnakes > 0) {
+            int startPosition = sc.nextInt();
+            int endPosition = sc.nextInt();
+            levels.setSnake(startPosition, endPosition);
+            numberOfSnakes--;
+        }
+        int numberOfLadders = sc.nextInt();
+        while (numberOfLadders > 0) {
+            int startPosition = sc.nextInt();
+            int endPosition = sc.nextInt();
+            levels.setLadders(startPosition, endPosition);
+            numberOfLadders--;
+        }
+        int numberOfPlayers = sc.nextInt();
+        int index = 0;
+        while (numberOfPlayers > 0) {
+            String player = sc.next();
+            levels.setPlayes(index, player);
+            numberOfPlayers--;
+            index++;
+        }
+        Play play = new Play(6);
+        System.out.println(play.Simulate() + "wins the game");
     }
 }
