44701f8e251d07d9153028b3c419a565e304c714
Maaz <maazshuaib18@gmail.com>
2023-06-14 20:53:55 +0530

    ignore files

---
+++ src/SnakeAndLadder/service/Play.java
@@ -0,0 +1,11 @@
+package SnakeAndLadder.service;
+
+import SnakeAndLadder.model.Dice;
+import SnakeAndLadder.model.Levels;
+import SnakeAndLadder.model.Player;
+import SnakeAndLadder.model.Position;
+import java.util.HashMap;
+
+public class Play {
+
+}
